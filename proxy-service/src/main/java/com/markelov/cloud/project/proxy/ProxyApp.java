b49086c117290c6664e7aef6ff476204bcc79be0
За Орду <markelovstass.93@mail.ru>
2021-03-23 19:36:03 +0300

    add project

--- proxy-service/src/main/java/com/markelov/cloud/project/proxy/ProxyApp.java
+++ proxy-service/src/main/java/com/markelov/cloud/project/proxy/ProxyApp.java
@@ -16,13 +16,15 @@ public class ProxyApp {
         SpringApplication.run(ProxyApp.class);
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer(){
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "PATCH");
-//            }
-//        };
-//    }
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+        }
+    };
+}
 }
